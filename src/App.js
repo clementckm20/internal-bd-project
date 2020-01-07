e753537dc100e30ab47bbfbaa2240828a361e81a
clement <chankinmingclement@gmail.com>
2020-01-07 16:29:19 +1100

    adding URL encoder to handle query params

--- src/App.js
+++ src/App.js
@@ -3,26 +3,83 @@ import Search from './Search.js';
 import logo from './logo.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='caption'>
-          Type keywords to search for BD projets.
-        </p>
-				<div className='row'>
-					<div className='form-box'>
-						<form className="form-inline" action="action_page.php">
-							<div className='col-xs-10'><input className='search' placeholder='e.g. Agile, Digital Enabler, Richard'/></div>
-							<div className='col-xs-2'><button className='searchbtn' type="submit"><i className="fa fa-search"></i></button></div>
-						</form>
+
+class App extends React.Component {
+	constructor(props) {
+  	super(props);
+		this.state = {
+			keywords: '',
+			result: [{
+				fields: {
+					proposal_talking_deck: 'test'
+				}
+			}]
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.loadAllObjectsInfo = this.loadAllObjectsInfo.bind(this);
+		this.parseQuery = this.parseQuery.bind(this);
+	}
+
+	handleChange (evt) {
+		const name = evt.target.name;
+		const value = evt.target.value;
+		this.setState({ [name]: value });
+	}
+
+	parseQuery(queryString) {
+	    var query = {};
+	    var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+	    for (var i = 0; i < pairs.length; i++) {
+	        var pair = pairs[i].split('=');
+	        query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+	    }
+	    return query;
+	}
+	// headers set to be Content-Type because of type CORS which makes some headers restricted
+
+
+	async loadAllObjectsInfo(evt, queryParams) {
+		evt.preventDefault();
+		const searchParams = new URLSearchParams({ q: queryParams});
+		// console.log(searchParams.toString())
+    const requestOptions = {
+        method: 'GET',
+        // headers: {
+				// 		"Access-Control-Allow-Origin": "*",
+		    //     "Access-Control-Allow-Headers": "Content-Type",
+		    //     // "Access-Control-Allow-Methods": "GET",
+				// 		'Content-Type': 'application/json',
+        // },
+    };
+
+    let response = await fetch(`https://o2on8l1jcj.execute-api.ap-southeast-2.amazonaws.com/search/?${searchParams.toString()}`, requestOptions);
+
+    let data = await response.json();
+    this.setState({ result: data.hits.hit });
+  }
+
+	render() {
+		console.log(this.state)
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p className='caption'>
+						Type keywords to search for BD projets.
+					</p>
+					<div className='row'>
+						<div className='form-box'>
+							<form className="form-inline" action="action_page.php">
+								<div className='col-xs-10'><input name='keywords' onChange={(evt) => this.handleChange(evt)} className='search' placeholder='e.g. Agile, Digital Enabler, Richard'/></div>
+								<div className='col-xs-2'><button onClick={(evt)=>this.loadAllObjectsInfo(evt, this.state.keywords)} className='searchbtn'><i className="fa fa-search"></i></button></div>
+							</form>
+						</div>
 					</div>
-				</div>
-				<Search />
-      </header>
-    </div>
-  );
+					<Search result={this.state.result} />
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/Search.js
+++ src/Search.js
@@ -6,55 +6,20 @@ class App extends React.Component {
 	constructor(props) {
   	super(props);
 		this.state = {
-			result: {
-				hits: {
-					hit: [{
-						fields: {
-							proposal_talking_deck: 'test'
-						}
-					}]
-				}
-			}
 		}
-	  this.loadAllObjectsInfo = this.loadAllObjectsInfo.bind(this);
 	}
-
-	async loadAllObjectsInfo(endpoint) {
-		// headers set to be Content-Type because of type CORS which makes some headers restricted
-    const requestOptions = {
-        method: 'GET',
-        // headers: {
-				// 		"Access-Control-Allow-Origin": "*",
-		    //     "Access-Control-Allow-Headers": "Content-Type",
-		    //     // "Access-Control-Allow-Methods": "GET",
-				// 		'Content-Type': 'application/json',
-        // },
-    };
-		const q= 'david'
-    let response = await fetch(`https://o2on8l1jcj.execute-api.ap-southeast-2.amazonaws.com/search/?q=${q}`, requestOptions);
-
-    let data = await response.json();
-    this.setState({ [endpoint]: data });
-  }
-
-  componentDidMount (){
-		//Api calls
-    this.loadAllObjectsInfo('result')
-  }
-
   render() {
-		console.log(this.state)
 		const test_arr = [0,1,2,3,4,5]
 		return (
 			<div>
 				<p className='caption'>Result</p>
 				<div className='result'>
-				{ test_arr.map((i)=> {
+				{ this.props.result.map((i)=> {
 					return (
-						<div className="row">
-							<div key={i} className='card'>
+						<div key={i} className="row">
+							<div className='card'>
 								<div className='col-xs-8'>
-									<div className='row'><div className='card-row'>File Name:<span className='card-element'>{'ANZ '}{this.state.result.hits.hit[0].fields.proposal_talking_deck}</span></div></div>
+									<div className='row'><div className='card-row'>File Name:<span className='card-element'>{'ANZ '}{this.props.result[0].fields.proposal_talking_deck}</span></div></div>
 									<div className='row'><div className='card-row'>Categories:<span className='card-element'>{'Op Model, Transformation'}</span></div></div>
 									<div className='row'><div className='card-row'>Industry:<span className='card-element'>{'Banking'}</span></div></div>
 									<div className='row'><div className='card-row'>Owner:<span className='card-element'>{'David Williams'}</span></div></div>
